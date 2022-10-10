796c2bab8e79b32d7b0764c489f34543445da2b5
Abhishekshivam77 <shivamkmr37@gmail.com>
2022-10-10 20:03:48 +0530

    problem1

---
+++ problem1/problem2.js
@@ -0,0 +1,4 @@
+let x = ("Abhishek");
+x = ("Shyam Kumar");
+x = ("Poonam Gupta");
+console.log(x);
---
+++ problem1/problem3.js
@@ -0,0 +1,5 @@
+let x = ("Abhishek");
+let y = (22);
+console.log(x , y);
+console.log(typeof x);
+console.log(typeof y);
